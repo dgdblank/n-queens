e57e2bf86413b03e015d50df4b78f1dc5f8f09a7
Hack Reactor Students <accounts@hackreactor.com>
2015-05-19 12:32:28 -0700

    Completed up to findNsolutionsQueens

--- src/solvers.js
+++ src/solvers.js
@@ -19,7 +19,7 @@ window.findNRooksSolution = function(n) {
   var solution = new Board({'n': n});
   var rows=solution.rows();
   rows[0][0]=1;
-  var count = n;
+
   if(n===1){
     console.log('Single solution for ' + n + ' rooks:', JSON.stringify(rows));
     return rows;
@@ -45,7 +45,7 @@ window.countNRooksSolutions = function(n) {
     return 1;
   }else{
    while(count <= n){
-    debugger;
+
     solutionCount = solutionCount * count;
     count++;
    }
@@ -59,16 +59,110 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var col = 0;
+  var row = 0;
+  var solution = new Board({'n': n});
+  var rows = solution.rows();
+    if(n === 0){
+      console.log('Single solution for ' + n + ' queens:', JSON.stringify([]));
+      return [];
+    }
+    if(n===2){
+      console.log('Single solution for ' + n + ' queens:', JSON.stringify([[0,0],[0,0]]));
+      return [[0,0],[0,0]];
+    }
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+    if(n === 3){
+      console.log('Single solution for ' + n + ' queens:', JSON.stringify([[0,0,0],[0,0,0],[0,0,0]]));
+      return [[0,0,0],[0,0,0],[0,0,0]];
+    }
+
+    if(n === 1){
+      console.log('Single solution for ' + n + ' queens:', JSON.stringify([[1]]));
+      return [[1]];
+    }
+
+ var checkN = function(){
+    rows[row][col] = 1;
+
+    for(var i = 0; i<rows.length; i++){
+      for(var j = 0 ;j<rows.length;j++){
+        rows[i][j] = 1;
+        if(solution.hasAnyQueensConflicts()){
+          rows[i][j] = 0;
+        }
+        else{
+          break;
+        }
+      }
+    }
+
+    var numPieces = _.reduce(_.flatten(solution.rows()), function(a,b){ return a + b});
+    if(numPieces !== n && col < n){
+      col++;
+      solution=new Board({'n':n});
+      rows=solution.rows();
+      if(col === n){
+        row++;
+        col=0;
+      }
+      checkN();
+    }
+    // else if(numPieces !== n && col===n && row<n){
+    //   row++;
+    //   col=0;
+    //   solution=new Board({'n':n});
+    //   rows=solution.rows();
+    //   checkN();
+    // }
+
+
+    return rows;
+};
+var result=checkN();
+// debugger;
+console.log('Single solution for ' + n + ' queens:', JSON.stringify(result));
+  return result;
+
 };
 
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  if(n === 0){ return 1};
+
+  // board = 2d array
+  var checkN = function(board){
+    if(array.length === n){
+      var tempBoard = new Board(board);
+      if(!tempBoard.hasAnyQueensConflicts()){
+        solutionCount++;
+        return;
+      }
+    }
+    //while loop goes through every row
+    var bool=true;
+    var index = 0;
+    while(index < n && bool){
+      for(var i = 0; i < n; i++){
+        if(board.get(index)[i]){
+          bool=false;
+          break;
+        }
+      }
+      index++;
+    }
+      //if row contains 1 advance
+      //else store row index
+    for(var i = 0; i < n; i++){
+      var copy = Array.prototype.slice.call(board);
+
+  }
+
+
+
+};
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
